63ad7d67af8aa0cb2035d746c32faf0ca67bf578
Brian Tinsley <btinsley@usc.edu>
2022-08-22 20:54:56 -0700

    update readme

--- R/filtering_master_data.R
+++ R/filtering_master_data.R
@@ -1,5 +1,20 @@
 # filtering the data
 
+
+# filter keywords data
+
+keywords = read.csv("cmu_usc_pwg_mapped.csv")
+
+filter_keywords = function(data, threshold){
+  mini_df = data[data$weight > threshold, ]
+  return(mini_df)
+}
+
+filtered = filter_keywords(keywords, 0.5)
+# write.csv(filtered, "filtered_keywords", row.names=F)
+
+
+# filter the master data by sdgs-- dont map to and sdg if the weight
 master_data = read.csv("classes_total_weight.csv")
 
 filter_master_data = function(data, threshold){
@@ -8,8 +23,7 @@ filter_master_data = function(data, threshold){
 }
 
 
-filter_master_data_column = function(data, threshold, column){
-  df = data[data["weight"] >= threshold, ]
-}
+# filter master data by total weight
+
 
 
--- R/total_weight_generator.R
+++ R/total_weight_generator.R
@@ -9,48 +9,48 @@ master_data = read.csv("master_course_sdg_data.csv")
 # read in the USC course data
 courses = read.csv("usc_courses.csv")
 classes = unique(courses$course_title)
-df = data.frame(classes)
+weights = data.frame(classes)
 
-df = df %>% add_column(sdg1_weight = NA)
-df = df %>% add_column(sdg2_weight = NA)
-df = df %>% add_column(sdg3_weight = NA)
-df = df %>% add_column(sdg4_weight = NA)
-df = df %>% add_column(sdg5_weight = NA)
-df = df %>% add_column(sdg6_weight = NA)
-df = df %>% add_column(sdg7_weight = NA)
-df = df %>% add_column(sdg8_weight = NA)
-df = df %>% add_column(sdg9_weight = NA)
-df = df %>% add_column(sdg10_weight = NA)
-df = df %>% add_column(sdg11_weight = NA)
-df = df %>% add_column(sdg12_weight = NA)
-df = df %>% add_column(sdg13_weight = NA)
-df = df %>% add_column(sdg14_weight = NA)
-df = df %>% add_column(sdg15_weight = NA)
-df = df %>% add_column(sdg16_weight = NA)
-df = df %>% add_column(sdg17_weight = NA)
+weights = weights %>% add_column(sdg1_weight = NA)
+weights = weights %>% add_column(sdg2_weight = NA)
+weights = weights %>% add_column(sdg3_weight = NA)
+weights = weights %>% add_column(sdg4_weight = NA)
+weights = weights %>% add_column(sdg5_weight = NA)
+weights = weights %>% add_column(sdg6_weight = NA)
+weights = weights %>% add_column(sdg7_weight = NA)
+weights = weights %>% add_column(sdg8_weight = NA)
+weights = weights %>% add_column(sdg9_weight = NA)
+weights = weights %>% add_column(sdg10_weight = NA)
+weights = weights %>% add_column(sdg11_weight = NA)
+weights = weights %>% add_column(sdg12_weight = NA)
+weights = weights %>% add_column(sdg13_weight = NA)
+weights = weights %>% add_column(sdg14_weight = NA)
+weights = weights %>% add_column(sdg15_weight = NA)
+weights = weights %>% add_column(sdg16_weight = NA)
+weights = weights %>% add_column(sdg17_weight = NA)
 
 # get the sum of the weights for each sdg
-for (class in df$classes){
-  mini_df = unique(master_data[master_data$course_title == class, c("keyword", "goal", "weight", "course_title") ])
-  # print(mini_df)
-  unique_goals = unique(mini_df$goal)
+for (class in weights$classes){
+  mini_weights = unique(master_data[master_data$course_title == class, c("keyword", "goal", "weight", "course_title") ])
+  # print(mini_weights)
+  unique_goals = unique(mini_weights$goal)
   for (g in unique_goals){
-    goal_df = mini_df[mini_df$goal == g, ]
-    # print(goal_df)
-    goal_sum = sum(goal_df$weight)
+    goal_weights = mini_weights[mini_weights$goal == g, ]
+    # print(goal_weights)
+    goal_sum = sum(goal_weights$weight)
     # print(goal_sum)
     col_number= 1 + g
-    df[df$classes == class, col_number] = goal_sum
+    weights[weights$classes == class, col_number] = goal_sum
   }
 }
 
 # get the total sum and add last column
-df = df %>% add_column(total_weight = NA)
-for (i in nrow(df)){
-  df$total_weight = rowSums(df[,-1], na.rm=TRUE) #exlude first two columns
+weights = weights %>% add_column(total_weight = NA)
+for (i in nrow(weights)){
+  weights$total_weight = rowSums(weights[,-1], na.rm=TRUE) #exlude first two columns
 }
 
 
-write.csv(df, "classes_total_weight.csv")
+write.csv(weights, "classes_total_weight.csv", row.names = F)
 
 
