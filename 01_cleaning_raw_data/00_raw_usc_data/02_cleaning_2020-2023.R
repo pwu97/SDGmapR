d9aea76f241947122ba5c8a1bf8f65a086f7484a
chenalison <alisonchen.y@gmail.com>
2023-06-30 16:58:27 -0700

    new course filtering + faster code

--- 01_cleaning_raw_data/00_raw_usc_data/02_cleaning_2020-2023.R
+++ 01_cleaning_raw_data/00_raw_usc_data/02_cleaning_2020-2023.R
@@ -2,114 +2,109 @@
 # different format from the 2018/2019 data
 
 # data = read.csv("All_SOC_files_2020-2022_fixed.csv")
-data = read.csv("combined_data.csv")
+data = read.csv("01_cleaning_raw_data/00_raw_usc_data/combined_data.csv")
+data$COURSE_TITLE <- trimws(data$COURSE_TITLE)
 
 # filtering out Master's Thesis, Directed Research, and Doctoral Dissertation, all of which have a space at the end
 # also counting number of sections and number of enrolled students
 clean_data = function (raw_data){
-  data_clean = raw_data
   # we could split data in to origin < or > 20231
-  # data_clean = raw_data[trimws(raw_data$COURSE_TITLE) !=  "Directed Research"
-  #                       & trimws(raw_data$COURSE_TITLE) !=  "Master's Thesis" 
-  #                       & trimws(raw_data$COURSE_TITLE) !=  "Doctoral Dissertation"
-  #                       & trimws(raw_data$COURSE_TITLE) !=  "Research"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Studies for the Qualifying Examination"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Enrollment for Early-Arriving PhD Students"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Studies for the Qualifying Examination"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Studies for Master's Examination"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Internship for Curricular Practical Training"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Off-Campus Studies"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Creative Problem Solving 1"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Creative Problem Solving 2"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Postdoctoral Fellows"
-  #                       & trimws(raw_data$COURSE_TITLE) != "International Academy Holding Course"
-  #                       & trimws(raw_data$COURSE_TITLE) != "International Academy Holding Course (Part II)"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Test Course"
-  #                       & trimws(raw_data$COURSE_TITLE) != "TEMPORARY ENROLLMENT/FILL COURSE"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Consortium Enrollment (Financial Aid)"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Summer Enrollment for Continuing PhD Students"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Capstone"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Fieldwork Section II"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Organizational Development"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Collaborative Communication"
-  #                       & trimws(raw_data$COURSE_TITLE) != "Board Development"
-  #                       , ]
+  titles_containing = c("Directed Research",
+                        "Individual Instruction",
+                        "Graduate Certificate Performance",
+                        "Research Materials and Techniques",
+                        "Doctoral Dissertation",
+                        "Capstone",
+                        "Off-Campus Studies",
+                        "Overseas Block Enrollment",
+                        "Thesis",
+                        "Independent Study",
+                        "Field Internship Experience",
+                        "Individual Production Workshop",
+                        "Advanced Production Seminar",
+                        "Group Production Workshop",
+                        "Independent Studies",
+                        "Team Projects",
+                        "Independent Research",
+                        "Special Laboratory",
+                        "Advanced Producing Project",
+                        "Research Practicum")
+  titles_matching = c("Advanced Research Experience",
+                      "Board Development",
+                      "Collaborative Communication",
+                      "Consortium Enrollment (Financial Aid)",
+                      "Creative Problem Solving 2",
+                      "Enrollment for Early-Arriving PhD Students",
+                      "Fieldwork Section II",
+                      "International Academy Holding Course",
+                      "International Academy Holding Course (Part II)",
+                      "Internship for Curricular Practical Training",
+                      "Organizational Development",
+                      "Postdoctoral Fellows",
+                      "Research",
+                      "Studies for Master's Examination",
+                      "Studies for the Qualifying Examination",
+                      "Summer Enrollment for Continuing PhD Students",
+                      "TEMPORARY ENROLLMENT/FILL COURSE",
+                      "Test Course",
+                      "Dissertation in Practice",
+                      "Consulting Project",
+                      "Doctoral dissertation",
+                      "Dissertation Seminar",
+                      "Planning, Design and Development Professional Dissertation")
+  descriptions_containing = c("Directed undergraduate research",
+                              "Directed graduate research")
+  data_clean <- raw_data %>%
+    filter(!grepl(paste(titles_containing, collapse = "|"), COURSE_TITLE) & 
+             !COURSE_TITLE %in% titles_matching &
+             !grepl(paste(descriptions_containing, collapse = "|"), COURSE_DESCRIPTION) &
+             !grepl("-[47]90|-594", COURSE_CODE))
   # want to get a column for the total number of enrolled students across all sections for class
   # first grab all the unique courses per semester
-  data_unique = data_clean[!duplicated(data_clean[ , c("COURSE_CODE", "origin")]), ]
-  students = c()
-  sections = c()
-  for (i in 1:nrow(data_unique)){
-    course = data_unique[i, "COURSE_CODE"] 
-    term = data_unique[i, "origin"]
-    # grab just the data for that course/semester
-    mini_df = data_clean[ data_clean$COURSE_CODE == course & data_clean$origin == term , ]
-    # sum up the total students across all sections
-    total_students = sum(as.numeric(mini_df$TOTAL_ENR))
-    students = c(students, total_students)
-    # now get number of sections for each class in a semester
-    rows = nrow(mini_df)
-    sections = c(sections, rows)
-  }
-  data_unique["total_enrolled"] = students
-  data_unique["N.Sections"] = sections
-  
-  return (data_unique)
+  data_clean %>%
+    group_by(COURSE_CODE, origin) %>%
+    mutate(total_enrolled = sum(TOTAL_ENR),
+           N.Sections = n()) %>%
+    filter(row_number()==1)
 }
-# still need to cut out 700 level courses
 
-#takes awhile to run this line
 cleaned = clean_data(data)
 
 # now need to get the semester format right, need F20, SU20, SP20 etc.
 # right now it is in format 20202_SUMMER, 20212_SUMMER
 # 3 is fall, 2 is summer, 1 is spring
-get_semesters <- function(term_col){
-  semesters = c()
-  for (i in 1:length(term_col)){
-    letters = term_col[i]
-    year = substr(letters,3,4)
-    term = substr(letters, 5,5)
-    if (term == "1"){
-      term = "SP"
-    }
-    else if (term == "2"){
-      term = "SU"
-    }
-    else{
-      term = "F"
-    }
-    result = paste0(term, year)
-    semesters = c(semesters, result)
+get_semester <- function(x){
+  if (x%%10 == 3) {
+    # fall
+    paste("F", substr(as.character(x), 3, 4), sep = "")
+  } else if (x%%10 == 2) {
+    # summer
+    paste("SU", substr(as.character(x), 3, 4), sep = "")
+  } else if (x%%10 == 1) {
+    # spring
+    paste("SP", substr(as.character(x), 3, 4), sep = "")
   }
-  return(semesters)
 }
 
 # helper function to get "year" column in form AY20, AY21 etc.
 # parameter is the column of the terms F20, SP21, etc.
 # if it is 2020-2 or 2020-3, the AY is 21
 # if it is 2021-1, AY is 21
-get_year <- function(term_col){
-  years = c()
-  for (i in 1:length(term_col)){
-    letters = term_col[i]
-    # convert year to a integer
-    year = strtoi(substr(letters,3,4))
-    term = substr(letters, 5,5)
-    # summer and fall terms count as the academic year of spring
-    if (term == "2" | term == "3"){
-      year = year + 1
-    }
-    result = paste0("AY", year)
-    years = c(years, result)
+get_year <- function(letters){
+  # convert year to a integer
+  year = strtoi(substr(letters,3,4))
+  term = substr(letters, 5,5)
+  # summer and fall terms count as the academic year of spring
+  if (term == "2" | term == "3"){
+    year = year + 1
   }
-  return(years)
+  paste0("AY", year)
 }
 
 
 transform_data = function(course_data){
   # now create "semester" column in the classes dataframe
-  course_data$semester = get_semesters(course_data$origin)
+  course_data$semester = sapply(course_data$origin, get_semester)
   #create the "course_num" column from "rcl.class"
   course_data$section = course_data$SECTION
   #create the "course_desc" column
@@ -121,7 +116,7 @@ transform_data = function(course_data){
   # create department column
   course_data$department = course_data$DEPARTMENT #changed this from departmentowner name
   # create year column
-  course_data$year = get_year(course_data$origin)
+  course_data$year = sapply(course_data$origin, get_year)
   # select relevant columns for the Shiny App
   course_data = course_data[, c("courseID", "course_title", "semester", "section", "course_desc", "department", "N.Sections", "year", "total_enrolled")]
   
@@ -133,17 +128,11 @@ data_final = transform_data(cleaned)
 
 # also want to get "all semesters" column in the usc_courses dataframe
 get_all_semesters <- function(data) {
-  data$all_semesters = NA
-  for (i in 1:nrow(data)){
-    print(i)
-    # grab the course ID
-    course = data$courseID[i]
-    df = data[data$courseID == course, ]
-    sems = unique(df$semester)
-    result = paste(sems, collapse=", ")
-    data$all_semesters[i] = result
-  }
-  return (data)
+  # data$semester <- factor(data$semester, levels = c("SU20", "F20", "SP21", "SU21", "F21", "SP22", "SU22", "F22", "SP23", "SU23", "F23"))
+  data %>%
+    group_by(courseID) %>%
+    mutate(all_semesters = paste(unique(semester), collapse = ", ")) %>%
+    ungroup()
 }
 
 data_final = get_all_semesters(data_final)
@@ -151,31 +140,24 @@ data_final = get_all_semesters(data_final)
 
 # last thing!
 # helper function to get the grad/undergrad column from courseID column
-get_class_levels <- function(data) {
-  data$course_level = NA
-  for (i in 1:nrow(data)){
-    print(i)
-    # grab the course ID
-    course = data$courseID[i]
-    # get just the number after the "-"
-    num = strsplit(course, "-")[[1]][2]
-    # some course codes have letters at the end, remove them and convert to number
-    val = as.numeric(unlist(strsplit(gsub("[^[:digit:]. ]", "", num), " +")))
-    if (val >= 500){
-      data$course_level[i] = "graduate"
-    }
-    else if (val < 300){
-      data$course_level[i] = "undergrad lower division"
-    }
-    else{
-      data$course_level[i] = "undergrad upper division"
-    }
+get_course_level <- function(course) {
+  # get just the number after the "-"
+  num = strsplit(course, "-")[[1]][2]
+  # some course codes have letters at the end, remove them and convert to number
+  val = as.numeric(unlist(strsplit(gsub("[^[:digit:]. ]", "", num), " +")))
+  if (val >= 500){
+    "graduate"
   }
-  return (data)
+  else if (val < 300){
+    "undergrad lower division"
+  }
+  else {
+    "undergrad upper division"
+  }
 }
 
 # now add the course levels to data
-data_final = get_class_levels(data_final)
+data_final$course_level = sapply(data_final$courseID, get_course_level)
 
 write.csv(data_final, "usc_courses.csv",row.names = F)
 
